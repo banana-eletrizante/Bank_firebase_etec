e77edb70dcbf28ee6a65150e77b01f813c7b650f
André Rösler <157327079+banana-eletrizante@users.noreply.github.com>
2026-09-09 18:21:11 -0300

    docs: example Firebase environment shape

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'COLE_AQUI',
+    authDomain: 'seu-projeto.firebaseapp.com',
+    projectId: 'seu-projeto',
+    storageBucket: 'seu-projeto.appspot.com',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
+  }
+};
